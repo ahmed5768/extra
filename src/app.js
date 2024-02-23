90a2090a94c9670e4e4fe7e54c251f7e6198b12f
ahmed5768 <101082079+ahmed5768@users.noreply.github.com>
2024-02-24 03:28:05 +0500

    done

---
+++ src/app.js
@@ -0,0 +1,266 @@
+var budget = prompt("What is your Budget?", "low, medium, high")
+var system = prompt("which type of system do you prefer", "android, ios")
+if(budget === "high"){
+    if(system === "ios"){
+        var camera = prompt("what camera quality do you need?", "basic, moderate, high")
+        if(camera === "basic"){
+            alert("We have this model in basic camera quality \n iPhone 7")
+        }
+        else if(camera === "moderate"){
+            alert("We have this model in moderate camera quality \n iPhone 12promax")
+        }
+        else if(camera === "high"){
+            alert("We have this model in high camera quality \n iPhone 15promax")
+        }
+        else(
+            alert("Not Avalaible")
+        )
+        var battery = prompt("how important is battery life to you? ", "Short, average, long")
+        if(battery === "short"){
+            alert("We have this model in short battery life \n iPhone 4s")
+        }
+        else if(battery === "average"){
+            alert("We have this model in average battery life \n iPhone X")
+        }
+        else if(battery === "long"){
+            alert("We have this model in long battery life \n iPhone 13promax")
+        }
+        else{
+            alert("Not Available")
+        }
+        var screen = prompt("what screen size do you prefer", "small, medium, large")
+        if(screen === "small"){
+            alert("We have this model in small screen size \n iPhone SE")
+        }
+        else if(screen === "medium"){
+            alert("We have this model in medium screen size \n iPhone X")
+        }
+        else if(screen === "large"){
+            alert("We have this model in large screen size \n iPhone 14promax")
+        }
+        else{
+            alert("Not Avalaible")
+        }
+    }
+
+    if(system === "android"){
+        var camera = prompt("what camera quality do you need?", "basic, moderate, high")
+        if(camera === "basic"){
+            alert("We have this model in basic camera quality \n infinix")
+        }
+        else if(camera === "moderate"){
+            alert("We have this model in moderate camera quality \n realme")
+        }
+        else if(camera === "high"){
+            alert("We have this model in high camera quality \n samsung")
+        }
+        else(
+            alert("Not Avalaible")
+        )
+        var battery = prompt("how important is battery life to you? ", "Short, average, long")
+        if(battery === "short"){
+            alert("We have this model in short battery life \n samsung")
+        }
+        else if(battery === "average"){
+            alert("We have this model in average battery life \n huawei")
+        }
+        else if(battery === "long"){
+            alert("We have this model in long battery life \n infinix")
+        }
+        else{
+            alert("Not Available")
+        }
+        var screen = prompt("what screen size do you prefer", "small, medium, large")
+        if(screen === "small"){
+            alert("We have this model in small screen size \n nokia 3310")
+        }
+        else if(screen === "medium"){
+            alert("We have this model in medium screen size \n vigotel")
+        }
+        else if(screen === "large"){
+            alert("We have this model in large screen size \n itel flip1")
+        }
+        else{
+            alert("Not Avalaible")
+        }
+    }
+    
+   
+}
+else if(budget === "medium"){
+    if(system === "ios"){
+        var camera = prompt("what camera quality do you need?", "basic, moderate, high")
+        if(camera === "basic"){
+            alert("We have this model in basic camera quality \n iPhone 7")
+        }
+        else if(camera === "moderate"){
+            alert("We have this model in moderate camera quality \n iPhone 12promax")
+        }
+        else if(camera === "high"){
+            alert("We have this model in high camera quality \n iPhone 15promax")
+        }
+        else(
+            alert("Not Avalaible")
+        )
+        var battery = prompt("how important is battery life to you? ", "Short, average, long")
+        if(battery === "short"){
+            alert("We have this model in short battery life \n iPhone 4s")
+        }
+        else if(battery === "average"){
+            alert("We have this model in average battery life \n iPhone X")
+        }
+        else if(battery === "long"){
+            alert("We have this model in long battery life \n iPhone 13promax")
+        }
+        else{
+            alert("Not Available")
+        }
+        var screen = prompt("what screen size do you prefer", "small, medium, large")
+        if(screen === "small"){
+            alert("We have this model in small screen size \n iPhone SE")
+        }
+        else if(screen === "medium"){
+            alert("We have this model in medium screen size \n iPhone X")
+        }
+        else if(screen === "large"){
+            alert("We have this model in large screen size \n iPhone 14promax")
+        }
+        else{
+            alert("Not Avalaible")
+        }
+    }
+
+    if(system === "android"){
+        var camera = prompt("what camera quality do you need?", "basic, moderate, high")
+        if(camera === "basic"){
+            alert("We have this model in basic camera quality \n infinix")
+        }
+        else if(camera === "moderate"){
+            alert("We have this model in moderate camera quality \n realme")
+        }
+        else if(camera === "high"){
+            alert("We have this model in high camera quality \n samsung")
+        }
+        else(
+            alert("Not Avalaible")
+        )
+        var battery = prompt("how important is battery life to you? ", "Short, average, long")
+        if(battery === "short"){
+            alert("We have this model in short battery life \n samsung")
+        }
+        else if(battery === "average"){
+            alert("We have this model in average battery life \n huawei")
+        }
+        else if(battery === "long"){
+            alert("We have this model in long battery life \n infinix")
+        }
+        else{
+            alert("Not Available")
+        }
+        var screen = prompt("what screen size do you prefer", "small, medium, large")
+        if(screen === "small"){
+            alert("We have this model in small screen size \n nokia 3310")
+        }
+        else if(screen === "medium"){
+            alert("We have this model in medium screen size \n vigotel")
+        }
+        else if(screen === "large"){
+            alert("We have this model in large screen size \n itel flip1")
+        }
+        else{
+            alert("Not Avalaible")
+        }
+    }
+    
+   
+}
+else if(budget === "low"){
+    if(system === "ios"){
+        var camera = prompt("what camera quality do you need?", "basic, moderate, high")
+        if(camera === "basic"){
+            alert("We have this model in basic camera quality \n iPhone 7")
+        }
+        else if(camera === "moderate"){
+            alert("We have this model in moderate camera quality \n iPhone 12promax")
+        }
+        else if(camera === "high"){
+            alert("We have this model in high camera quality \n iPhone 15promax")
+        }
+        else(
+            alert("Not Avalaible")
+        )
+        var battery = prompt("how important is battery life to you? ", "Short, average, long")
+        if(battery === "short"){
+            alert("We have this model in short battery life \n iPhone 4s")
+        }
+        else if(battery === "average"){
+            alert("We have this model in average battery life \n iPhone X")
+        }
+        else if(battery === "long"){
+            alert("We have this model in long battery life \n iPhone 13promax")
+        }
+        else{
+            alert("Not Available")
+        }
+        var screen = prompt("what screen size do you prefer", "small, medium, large")
+        if(screen === "small"){
+            alert("We have this model in small screen size \n iPhone SE")
+        }
+        else if(screen === "medium"){
+            alert("We have this model in medium screen size \n iPhone X")
+        }
+        else if(screen === "large"){
+            alert("We have this model in large screen size \n iPhone 14promax")
+        }
+        else{
+            alert("Not Avalaible")
+        }
+    }
+
+    if(system === "android"){
+        var camera = prompt("what camera quality do you need?", "basic, moderate, high")
+        if(camera === "basic"){
+            alert("We have this model in basic camera quality \n infinix")
+        }
+        else if(camera === "moderate"){
+            alert("We have this model in moderate camera quality \n realme")
+        }
+        else if(camera === "high"){
+            alert("We have this model in high camera quality \n samsung")
+        }
+        else(
+            alert("Not Avalaible")
+        )
+        var battery = prompt("how important is battery life to you? ", "Short, average, long")
+        if(battery === "short"){
+            alert("We have this model in short battery life \n samsung")
+        }
+        else if(battery === "average"){
+            alert("We have this model in average battery life \n huawei")
+        }
+        else if(battery === "long"){
+            alert("We have this model in long battery life \n infinix")
+        }
+        else{
+            alert("Not Available")
+        }
+        var screen = prompt("what screen size do you prefer", "small, medium, large")
+        if(screen === "small"){
+            alert("We have this model in small screen size \n nokia 3310")
+        }
+        else if(screen === "medium"){
+            alert("We have this model in medium screen size \n vigotel")
+        }
+        else if(screen === "large"){
+            alert("We have this model in large screen size \n itel flip1")
+        }
+        else{
+            alert("Not Avalaible")
+        }
+    }
+    
+   
+}
+else(
+    alert("Not Avalaible")
+)
